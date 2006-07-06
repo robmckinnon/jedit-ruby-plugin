66df3514c912fe48d663c580b4778ab3845afe34
robmckinnon <robmckinnon@6b1eeb88-9816-0410-afa2-b43733a0f04e>
2006-07-06 23:31:14 +0000

    new ri dir

--- ri/convert.rb
+++ ri/convert.rb
@@ -1,6 +1,6 @@
 require 'rdoc_to_java.rb'
 
-base_dir = "/home/b/apps/versions/jedit/4.2/plugins/RubyPlugin/ri/"
+base_dir = "/home/c/apps/jedit/plugins/RubyPlugin/ri/"
 result_dir = base_dir + "java-xml"
 serializer = JavaXmlSerializer.new(base_dir, result_dir)
 serializer.convert_dir(base_dir, "1.8/system")
