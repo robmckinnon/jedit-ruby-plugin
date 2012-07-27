f7ef2ec54ca73245bab3b94e92e7ad1aa4b92659
robmckinnon <rob@theyworkforyou.co.nz>
2012-07-27 09:10:11 +0100

    improved required args message

--- src/org/jedit/ruby/ri/XmlToBinary.java
+++ src/org/jedit/ruby/ri/XmlToBinary.java
@@ -304,7 +304,7 @@ public final class XmlToBinary {
 
     public static void main(String[] args) {
       if (args.length < 2) {
-        throw new RuntimeException("shit");
+        throw new RuntimeException("required args: inputPath resultPath");
       } else {
         System.out.println(args[0]);
         System.out.println(args[1]);
